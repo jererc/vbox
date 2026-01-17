31d0ffd7254d68d3c5c1555599bfd77ea4f72455
jererc <jererc@gmail.com>
2026-01-17 07:55:03 +0400

    do epic things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vbox',
-    version='2026.01.17.070837',
+    version='2026.01.17.075502',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/vbox',
