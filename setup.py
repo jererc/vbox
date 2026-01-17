dac825c8ca1876a2e48fb1c9358949cbb710a42b
jererc <jererc@gmail.com>
2026-01-17 07:08:38 +0400

    do epic things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vbox',
-    version='2026.01.17.070218',
+    version='2026.01.17.070837',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/vbox',
--- vbox/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from svcutils.service import get_work_dir, setup_logging
-
-NAME = 'bodiez'
-WORK_DIR = get_work_dir(NAME)
-setup_logging(path=WORK_DIR, name=NAME)
-logging.getLogger('asyncio').setLevel(logging.INFO)
